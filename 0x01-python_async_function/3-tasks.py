b7c5cf2e5a857cc59f5399ee8141e1f70759c692
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-01 17:10:08 -0500

    wc: work in progress

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+'''3. Tasks'''
+from typing import Callable
+import asyncio
+
+wait_random: Callable = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    task: asyncio.Task = asyncio.create_task(wait_random(max_delay))
+    return task
