c4c6a50bb1bcc205b789813922456bb820290f6d
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-01 17:17:59 -0500

    4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+'''4. Tasks'''
+from typing import Callable, List
+import asyncio
+
+task_wait_random: Callable = __import__('4-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''Return tasks as completed in that order'''
+    tasks: List[asyncio.Task] = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
