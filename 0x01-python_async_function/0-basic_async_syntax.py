0e673b5ecabbda1da627bb03361ab7e60b8ea3ad
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-01 12:34:58 -0500

    wc: work in progress

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 '''0. The basics of async'''
 
+import asyncio
 from random import uniform
-from asyncio import sleep
 
 
 async def wait_random(max_delay: int = 10) -> float:
     '''Return delay'''
     delay: float = uniform(0, max_delay)
-    await sleep(delay)
+    await asyncio.sleep(delay)
     return delay
