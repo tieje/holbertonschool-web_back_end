ec143893d6b6ec3febc455e536c1f4c4a4871e20
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-02 08:55:19 -0500

    completed tasks

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 '''1. Async Comprehensions'''
+from typing import Callable, List
+
+
+async_generator: Callable = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    '''Return a list of floats from async_generator()'''
+    return [i async for i in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 '''2. Run time for four parallel comprehensions'''
+
+from typing import Callable
+import asyncio
+from time import perf_counter
+async_comprehension: Callable = __import__(
+    '1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    start: float = perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end: float = perf_counter()
+    return end - start
