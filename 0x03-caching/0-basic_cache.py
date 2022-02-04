25ff7152ebc185d089a2717dc5e5bc773fc9b2b0
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-04 09:58:17 -0500

    added get to Basic Cache

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -11,3 +11,9 @@ class BasicCache(BaseCaching):
         if BaseCaching.checkArgsIsNone(key, item):
             return
         self.cache_data[key] = item
+
+    def get(self, key: str) -> str:
+        '''Get value of key from cache dict'''
+        if BaseCaching.checkArgsIsNone(key) or key not in self.cache_data:
+            return 'None'
+        return self.cache_data[key]
