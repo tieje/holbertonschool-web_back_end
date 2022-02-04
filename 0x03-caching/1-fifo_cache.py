6746ec913b5b4719ed0ae4bef9c7bf7dde149fd1
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-04 10:19:01 -0500

    renamed input order to be in character range limit

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''1. FIFO caching'''
 from base_caching import BaseCaching
-from typing import Union
+from typing import List, Union
 
 
 class FIFOCache(BaseCaching):
@@ -10,19 +10,19 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         '''Add input Order to keep track of order'''
         super().__init__()
-        self.__inputOrder = []
+        self.__Order: List[str] = []
 
     def put(self, key: str, item: str) -> None:
         '''
         Assign cache key to value
         If max items is reached, the first item put in will be removed.
         '''
-        if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.__inputOrder:
-            lastKey: str = self.__inputOrder.pop(0)
+        if len(self.cache_data) >= self.MAX_ITEMS and key not in self.__Order:
+            lastKey: str = self.__Order.pop(0)
             del self.cache_data[lastKey]
             print('DISCARD: ' + lastKey)
         if key and item:
-            self.__inputOrder.append(key)
+            self.__Order.append(key)
             self.cache_data[key] = item
 
     def get(self, key: str) -> Union[str, None]:
