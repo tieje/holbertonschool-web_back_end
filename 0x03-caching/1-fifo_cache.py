c691da404302548d0d9bc59efd352397f259d9ce
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-04 10:10:34 -0500

    updated put

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''1. FIFO caching'''
 from base_caching import BaseCaching
+from typing import Union
 
 
 class FIFOCache(BaseCaching):
@@ -11,10 +12,15 @@ class FIFOCache(BaseCaching):
         Assign cache key to value
         If max items is reached, the first item put in will be removed.
         '''
-        if BaseCaching.checkArgsIsNone(key, item):
-            return
-        self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             firstKey: str = list(self.cache_data.keys())[0]
             print('DISCARD: ' + firstKey)
             del self.cache_data[firstKey]
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key: str) -> Union[str, None]:
+        '''Get value of key from cache dict'''
+        if not key or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
