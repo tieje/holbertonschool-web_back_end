45ce177ade1a3d1a44d5e25adf02b89266c9e84a
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-04 10:14:32 -0500

    added a queue to fifo

--- 0x03-caching/1-fifo_cache.py
+++ 0x03-caching/1-fifo_cache.py
@@ -7,16 +7,22 @@ from typing import Union
 class FIFOCache(BaseCaching):
     '''FIFO Cache'''
 
+    def __init__(self):
+        '''Add input Order to keep track of order'''
+        super().__init__()
+        self.__inputOrder = []
+
     def put(self, key: str, item: str) -> None:
         '''
         Assign cache key to value
         If max items is reached, the first item put in will be removed.
         '''
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            firstKey: str = list(self.cache_data.keys())[0]
-            print('DISCARD: ' + firstKey)
-            del self.cache_data[firstKey]
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key not in self.__inputOrder:
+            lastKey: str = self.__inputOrder.pop(0)
+            del self.cache_data[lastKey]
+            print('DISCARD: ' + lastKey)
         if key and item:
+            self.__inputOrder.append(key)
             self.cache_data[key] = item
 
     def get(self, key: str) -> Union[str, None]:
