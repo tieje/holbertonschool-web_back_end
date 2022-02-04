85a0a233b44d9f33476357a034440fb7a65e8bdb
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-04 10:28:12 -0500

    updated types, names, get, and put

--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''3. LRU Caching'''
+from typing import List, Union
 from base_caching import BaseCaching
 
 
@@ -9,28 +10,27 @@ class LRUCache(BaseCaching):
     def __init__(self):
         '''Add input Order to keep track of order'''
         super().__init__()
-        self.__inputOrder = []
+        self.__Order: List[str] = []
 
     def put(self, key: str, item: str) -> None:
         '''
         Assign cache key to value
         If max items is reached, the most recently used item will be removed.
         '''
-        if BaseCaching.checkArgsIsNone(key, item):
-            return
-        if len(self.cache_data) >= self.MAX_ITEMS and key not in self.__inputOrder:
-            LastKey: str = self.__inputOrder.pop(0)
+        if len(self.cache_data) >= self.MAX_ITEMS and key not in self.__Order:
+            LastKey: str = self.__Order.pop(0)
             del self.cache_data[LastKey]
             print('DISCARD: ' + LastKey)
-        if key in self.__inputOrder:
-            self.__inputOrder.remove(key)
-        self.__inputOrder.append(key)
-        self.cache_data[key] = item
+        if key and item:
+            if key in self.__Order:
+                self.__Order.remove(key)
+            self.__Order.append(key)
+            self.cache_data[key] = item
 
-    def get(self, key: str) -> str:
+    def get(self, key: str) -> Union[str, None]:
         '''Get value of key from cache dict'''
-        if BaseCaching.checkArgsIsNone(key) or key not in self.cache_data:
-            return 'None'
-        self.__inputOrder.remove(key)
-        self.__inputOrder.append(key)
+        if not key or key not in self.cache_data:
+            return None
+        self.__Order.remove(key)
+        self.__Order.append(key)
         return self.cache_data[key]
