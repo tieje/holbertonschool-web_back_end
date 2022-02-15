6ad63008f524c83dc2e9329dff951a2458e37b45
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-15 10:51:27 -0500

    fixing auth

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -15,13 +15,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth_type = getenv("AUTH_TYPE")
+auth = None
 
 if auth_type == "auth":
     auth = Auth()
 elif auth_type == "basic_auth":
     auth = BasicAuth()
-else:
-    auth = None
 
 
 @app.errorhandler(404)
