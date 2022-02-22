09456fd751dd65ff3dbc9d6d029d3eea2085de97
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-22 10:13:43 -0500

    got up to task 7 today

---
+++ 0x08-user_authentication_service/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''basic flask app to return a json payload'''
+from urllib import request
+from flask import Flask, jsonify, request
+from auth import Auth
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def root() -> str:
+    ''' root route '''
+    return jsonify({'message': 'Bienvenue'})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    '''Users route'''
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({
+            'email': email,
+            'message': 'user created'
+        })
+    except ValueError:
+        return jsonify({
+            'message': 'email already registered'
+        }), 400
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''Auth module'''
+from db import DB
+from bcrypt import hashpw, gensalt
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def _hash_password(password: str) -> bytes:
+    return hashpw(password, gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self) -> None:
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''Return user object'''
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, str(_hash_password(password)))
+        raise ValueError('User ' + email + ' already exists')
