848c807bb0bbab84ffdc1ecd36ae1d24a1022c31
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-02-23 16:55:31 -0500

    wc: work in progress

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 '''Auth module'''
 from db import DB
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
+    '''Return Hash password'''
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+def _generate_uuid() -> str:
+    '''Generate uuid'''
+    return str(uuid4())
 
 
 class Auth:
@@ -24,3 +31,11 @@ class Auth:
             raise ValueError('User ' + email + ' already exists')
         except NoResultFound:
             return self._db.add_user(email, str(_hash_password(password)))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        '''Valid Login'''
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return checkpw(password.encode('utf-8'), user.hashed_password)
--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -2,6 +2,7 @@
 '''User authentication'''
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+
 Base = declarative_base()
 
 
