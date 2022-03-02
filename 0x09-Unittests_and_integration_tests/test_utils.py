c6fca97e1a779402ab287ba15696a8b2bc0cc66b
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-03-02 09:20:28 -0500

    wc: work in progress

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -10,11 +10,11 @@ class TestAccessNestedMap(unittest.TestCase):
     '''Test Access Nested Map'''
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        '''Test normal function'''
+        ''' test access nested map '''
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
