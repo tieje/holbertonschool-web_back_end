a90130942787f49f6d0203bb87ac259daec70d36
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-03-02 09:25:16 -0500

    wc: work in progress

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,12 +1,8 @@
 #!/bin/env python3
 '''Test client functions'''
-# from typing import Dict, Mapping, Sequence
 import unittest
-# from unittest.mock import Mock
 from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
-from nose.tools import assert_equal# , raises
-
 from client import GithubOrgClient
 
 
@@ -32,4 +28,4 @@ class TestGithubOrgClient(unittest.TestCase):
         mock = 'client.GithubOrgClient.org'
         with patch(mock, PropertyMock(return_value=val)):
             response = GithubOrgClient(expected)._public_repos_url
-            assert_equal(response, url)
+            self.assertEqual(response, url)
